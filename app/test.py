d1223dbb3993a9ca3c6f985b6932fb1b5499d5a8
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-05-11 17:25:48 +0530

    Added tracing in pipeline

---
+++ app/test.py
@@ -0,0 +1,20 @@
+from core.pipeline import UnifiedPipeline
+from prompts.prompt_loader import load_prompt
+from core.document_store import get_document_store
+
+
+def run_bias(document_store, bias_text):
+    prompt = load_prompt("bias.txt")
+    pipeline = UnifiedPipeline(document_store, prompt)
+    return pipeline.run("Evaluate whether the proposal should pass or not based on the bias criteria and the provided context.", bias_text)
+
+
+
+result = run_bias(
+            document_store=get_document_store(),
+            bias_text=f"""
+            The proposal is to implement a new feature that allows users to customize their profiles with themes and backgrounds.
+            """
+        )
+
+print(result)
